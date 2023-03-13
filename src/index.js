ecd3444cca0b108f7bc83c44d612c0bd8603c815
petriscreating <marialamo2010@gmail.com>
2023-03-13 12:16:12 +0100

    Fixing routes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
-    <React.StrictMode>
+  <React.StrictMode>
       <App />
-    </React.StrictMode>
-  );
+  </React.StrictMode>,
+  document.getElementById('root')
+);
   
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
