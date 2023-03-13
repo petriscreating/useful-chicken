df78236d27b2c6dbd3247baa96fe14b147bd85c6
petriscreating <marialamo2010@gmail.com>
2023-03-13 11:32:50 +0100

    Cleaning dependencies

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
     <React.StrictMode>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <App />
       </BrowserRouter>  
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
   );
   
 // If you want your app to work offline and load faster, you can change
